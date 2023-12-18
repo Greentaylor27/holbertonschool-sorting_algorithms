e95950ea11b841614fbc54bf6bbd2506d6f37fdf
taylor green <7196@holbertonstudents.com>
2023-12-18 12:05:45 -0600

    commiting first iteration of bubble sort function. Updating header file

--- bubble_sort.c
+++ bubble_sort.c
@@ -29,7 +29,7 @@ void bubble_sort(int *array, size_t size)
                 print_array(array, size);
             }
         }
-        if (swapped == NULL)
+        if (!swapped)
             break;
     }
 }
